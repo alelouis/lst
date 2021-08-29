1e23f0ba5702a4935b5cf032191fa2a11d5b59dc
alelouis <alelouis.dev@gmail.com>
2021-08-29 14:40:41 +0200

    refactor into module and comments

---
+++ src/lst.rs
@@ -0,0 +1,46 @@
+use hifitime::{Epoch};
+use chrono::prelude::*;
+use std::str::FromStr;
+
+pub fn deg_to_hms(deg:f64) -> (f64, f64, f64) {
+    // Convert degrees to (hour, minutes, seconds)
+    let time = (deg % 360.) * 24. / 360.;
+    let gmst_h = time.floor();
+    let gmst_m = (time*60.) % 60.;
+    let gmst_s = (time*3600.) % 60.;
+    (gmst_h, gmst_m, gmst_s)
+}
+
+pub fn utc_str() -> String {
+    // Return UTC hour in String format
+    let format = "%Y-%m-%dT%H:%M:%S%.6f UTC";
+    let utc_string: String = Utc::now().format(format).to_string();
+    utc_string
+}
+
+pub fn jd(utc_string: &String) -> f64 {
+    // Returns julian date for a given UTC String
+    let utc: Epoch = Epoch::from_str(&utc_string).unwrap();
+    let jd_utc: f64 = utc.as_jde_utc_days();
+    jd_utc
+}
+
+pub fn era(jd_ut1: f64) -> f64{
+    // Compute Earth Rotation Angle from julian date
+    let t_u = jd_ut1 - 2451545.0;
+    let era_rad = 2.*std::f64::consts::PI*(0.7790572732640 + 1.00273781191135448 * t_u);
+    let era_deg = era_rad * 180. / std::f64::consts::PI;
+    era_deg
+}
+
+pub fn lst_at_lon(lon: f64, gmst: f64) -> f64{
+    // Compute local sidereal angle for a given longitude
+    let lst = gmst + lon;
+    lst
+}
+
+pub fn print_lst(lst: f64) {
+    // Print an LST in hours:minutes:seconds format
+    let (gmst_h, gmst_m, gmst_s) = deg_to_hms(lst);
+    println!("LST:\t{:0>2.0}:{:0>2.0}:{:0>2.0}", gmst_h, gmst_m, gmst_s);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,13 @@
 extern crate hifitime;
 extern crate chrono;
-extern crate divrem;
 
-use hifitime::{Epoch};
-use chrono::prelude::*;
-use std::str::FromStr;
-use std::{thread, time};
-
-fn deg_hms(deg:f64) -> (f64, f64, f64) {
-    let time = (deg % 360.) * 24. / 360.;
-    let gmst_h = time.floor();
-    let gmst_m = (time*60.) % 60.;
-    let gmst_s = (time*3600.) % 60.;
-    (gmst_h, gmst_m, gmst_s)
-}
+mod lst;
 
 fn main() {
-    let sl = time::Duration::from_millis(100);
-
-    loop {
-        let utc_string: String = Utc::now().format("%Y-%m-%dT%H:%M:%S%.6f UTC").to_string();
-        print!("\x1B[2J\x1B[1;1H");
-        println!("UTC String:\t{}",utc_string);
-    
-        let utc: Epoch = Epoch::from_str(&utc_string).unwrap();
-        let jd_utc: f64 = utc.as_jde_utc_days();
-        println!("Julian Date:\t{:?}", jd_utc);
-    
-        let pi = std::f64::consts::PI;
-        let t_u = jd_utc - 2451545.0; // https://en.wikipedia.org/wiki/Universal_Time
-        let era_rad = 2.*pi*(0.7790572732640 + 1.00273781191135448 * t_u);
-        let era_deg = era_rad * 180. / pi;
-        println!("ERA Deg:\t{}", era_deg % 360.);
-    
-        let era_d = (era_deg % 360.).floor();
-        let era_h = era_deg % 360. / 15.;
-        let era_m = (era_deg*3600. / 60.) % 60.;
-        let era_s = era_deg*3600. % 60.;
-        println!("Earth Rotation:\t{} {:0>2.0}:{:0>2.0}:{:0>6.3}", era_d, era_h, era_m, era_s);
-
-        let gmst = era_deg.clone();
-        let (gmst_h, gmst_m, gmst_s) = deg_hms(gmst);
-        println!("GMST (appro.):\t{:0>2.0}:{:0>2.0}:{:0>2.0}", gmst_h, gmst_m, gmst_s);
-
-        let toulouse_long = 1.4442;
-        let toulouse_st = gmst + toulouse_long;
-        let (gmst_h, gmst_m, gmst_s) = deg_hms(toulouse_st);
-        println!("TLS (appro.):\t{:0>2.0}:{:0>2.0}:{:0>2.0}", gmst_h, gmst_m, gmst_s);
-    
-        thread::sleep(sl);
-    }
+    let tls_lon = 1.4333;
+    let utc_string = lst::utc_str();
+    let jd_ut1 = lst::jd(&utc_string);
+    let gmst = lst::era(jd_ut1);
+    let tls_lst = lst::lst_at_lon(tls_lon, gmst);
+    lst::print_lst(tls_lst);
 }
